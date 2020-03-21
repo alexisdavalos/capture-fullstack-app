73915ebb0906f4db637e1083ae07691863d0155d
Alexis <alexisdavalos.tech@gmail.com>
2020-03-21 19:37:38 -0400

    stage command and knexfile.js update

--- server/knexfile.js
+++ server/knexfile.js
@@ -2,20 +2,20 @@ require("dotenv").config(); //access to .env variables
 // Update with your config settings.
 const database = {
   client: process.env.CLIENT,
-      connection: {
-          host: process.env.LOCAL_HOST,
-          database: process.env.DEVELOPMENT_DB
-      },
-      migrations: {
-          directory: './data/migrations'
-      },
-      seeds: { 
-          directory: './data/seeds' 
-      },
-  };
+  connection: {
+    host: process.env.LOCAL_HOST,
+    database: process.env.DEVELOPMENT_DB
+  },
+  migrations: {
+    directory: "./data/migrations"
+  },
+  seeds: {
+    directory: "./data/seeds"
+  }
+};
 
 module.exports = {
-  development:{
+  development: {
     ...database,
     connection: {
       port: process.env.POSTGRES_PORT,
@@ -36,11 +36,14 @@ module.exports = {
     },
     tableName: process.env.MIGRATIONS_TABLE
   },
-
+  staging: {
+    ...database,
+    connection: process.env.DATABASE_URL,
+    tableName: process.env.MIGRATIONS_TABLE
+  },
   production: {
     ...database,
     connection: process.env.DATABASE_URL,
     tableName: process.env.MIGRATIONS_TABLE
   }
-
 };
